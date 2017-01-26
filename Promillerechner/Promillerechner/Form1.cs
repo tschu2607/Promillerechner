08f8857af54a4a9073b40102cf12557f2827562a
peterglatz <peterglatz@GLATZPETER>
2017-01-26 08:59:47 +0100

    Beda

--- Promillerechner/Promillerechner/Form1.cs
+++ Promillerechner/Promillerechner/Form1.cs
@@ -20,6 +20,7 @@ namespace Promillerechner
         private void button1_Click(object sender, EventArgs e)
         {
             button1.Enabled = false;
+            //Hofi stinkt
         }
     }
 }
